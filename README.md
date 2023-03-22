bb97b55196bfd27849506630ca5b8d4338876ede
marcocnc <marcocancelliere56@gmail.com>
2023-03-22 18:14:17 +0100

    output in page

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 1. Creare un ciclo di numeri che vanno da 1 a 100;
 2. In questo ciclo bisogna inserire delle condizioni in cui si fanno dei calcoli con il modulo;
 3. Nei multipli di 3 bisogna aggiungere la parola Fizz, nei multipli di 5 Buzz, nei multipli sia di 3 che di 5 FizzBuzz;
+4. Stampare sul DOM 
 
--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
+const container = document.querySelector('.container');
+console.log(container);
+
+
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0){
-        console.log("Fizz");
+
+    const box = document.createElement('div');
+    console.log(box);
+    box.classList.add('box');
+
+    if (i % 15 == 0){
+        console.log("FizzBuzz");
+        const fizzBuzz = document.createTextNode('FizzBuzz');
+        box.append(fizzBuzz);
+        box.classList.add('bg-yellow');
+        
     }else if (i % 5 == 0){
         console.log("Buzz");
+        const buzz = document.createTextNode('Buzz');
+        box.append(buzz);
+        box.classList.add('bg-green');
     } 
-    else if (i % 15 == 0){
-        console.log("FizzBuzz");
+    else if (i % 3 == 0){
+        console.log("Fizz");
+        const fizz = document.createTextNode('Fizz');
+        box.append(fizz);
+        box.classList.add('bg-blue');
     }else{
         console.log(i);
+        box.append(i);
     } 
+    container.append(box);
 }
-
 
